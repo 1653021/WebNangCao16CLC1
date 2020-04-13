914e4c5b40541e82b5dad3806a402460b3e1d4b0
1653021 <1653021@student.hcmus.edu.vn>
2020-04-13 18:49:59 +0700

    Fix host function.

---
+++ build/precache-manifest.d3bb7a782d8f29cd079992c7dea180d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7438f737581a420066a8b3cb6f2842",
+    "url": "/WebNangCao16CLC1/index.html"
+  },
+  {
+    "revision": "e48a97f032d028b34f45",
+    "url": "/WebNangCao16CLC1/static/css/main.0baa9e7e.chunk.css"
+  },
+  {
+    "revision": "c9a791c33144c12ea1cd",
+    "url": "/WebNangCao16CLC1/static/js/2.0da06914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebNangCao16CLC1/static/js/2.0da06914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48a97f032d028b34f45",
+    "url": "/WebNangCao16CLC1/static/js/main.f080c849.chunk.js"
+  },
+  {
+    "revision": "a43c074b96f8bfc771cf",
+    "url": "/WebNangCao16CLC1/static/js/runtime-main.f6be1574.js"
+  }
+]);
