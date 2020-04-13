14970687c422639abbc102c401cfe65ab5a4036c
1653021 <1653021@student.hcmus.edu.vn>
2020-04-13 18:45:24 +0700

    Modify homepage in package.json

---
+++ build/precache-manifest.af72af112e24ee66ef47bdbb6d1502a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d2a1c30749bf0a100c031a7ec7fabe",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/index.html"
+  },
+  {
+    "revision": "e48a97f032d028b34f45",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/static/css/main.0baa9e7e.chunk.css"
+  },
+  {
+    "revision": "c9a791c33144c12ea1cd",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/static/js/2.0da06914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/static/js/2.0da06914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48a97f032d028b34f45",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/static/js/main.f080c849.chunk.js"
+  },
+  {
+    "revision": "efb65d7ad10f980c7bd4",
+    "url": "/WebNangCao16CLC1/WebNangCao16CLC1/static/js/runtime-main.5dd3a234.js"
+  }
+]);
